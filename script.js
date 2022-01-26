a28e46b6897ac74d465fe65dd2559ec518eb90f5
NataliaRibeiroS <nethy.ribeiro10@gmail.com>
2022-01-26 14:53:55 -0300

    Codificando
    Inclusão do JS, HTML e CSS

---
+++ script.js
@@ -0,0 +1,25 @@
+//ESTRUTURA DE DADOS
+
+// Crie uma variável que receba um nome;
+let nome = "Natalia"
+
+// Crie uma variável que receba um número;
+let idade = 33
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let ano = 2022 
+let nascimento = ano - idade
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+let resultado = nascimento + idade
+console.log(resultado)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = false
+console.log(quartaFeira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof(quartaFeira))
